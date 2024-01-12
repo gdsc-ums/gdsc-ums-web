e0ecc7a444a54c1cbe0fd34d5a903fe8ac74078a
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2024-01-12 20:19:01 +0700

    refactor: home and about

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,10 +76,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "scale-up": {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(0.7)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "scale-up": "scale-up 1s ease-in-out infinite",
       },
     },
   },
